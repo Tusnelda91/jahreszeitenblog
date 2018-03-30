3b45ca735bae8f78275baab0b6cf41d113e36aaa
Jutta Lindekugel <julin@gmx.net>
2018-03-30 09:13:17 +0000

    Reihenfolge der Posts geändert

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,7 @@ class PostsController < ApplicationController
     before_action :authenticate_user!, except: [:index, :show]
     
     def index
-        @posts = Post.paginate(:page => params[:page], :per_page =>5)
+        @posts = Post.paginate(:page => params[:page], :per_page =>5).order('id ASC')
         #@posts = Post.all
     end
     
